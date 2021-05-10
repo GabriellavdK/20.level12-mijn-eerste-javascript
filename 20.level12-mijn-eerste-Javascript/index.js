fca38726f61e24143a1a52f84a08c06791912b8d
GabriellavdK <81296778+GabriellavdK@users.noreply.github.com>
2021-05-10 13:46:20 +0200

    Opdracht 20 geupload

---
+++ 20.level12-mijn-eerste-Javascript/index.js
@@ -0,0 +1,19 @@
+console.log("Hello Winc Academy")
+let naam = "Gabriella";
+console.log(naam);
+// let if = true; 
+// Je kunt van if geen variabele maken omdat if een keyword is in javascript. 
+console.log(4+4);
+console.log('4'+'4');
+console.log(10-5);
+console.log('10'-'5');
+console.log(4*4);
+console.log('4'*'4');
+console.log(16/4);
+console.log('16'/'4');
+console.log(40%4);
+console.log(40%9);
+let age = 48;
+console.log(typeof age);
+age = "48";
+console.log(typeof age);
